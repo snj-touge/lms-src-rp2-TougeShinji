f39547ac301ec38656991e6faa86ae03c4537e90
snj_toge <snj_toge@IBW-A25-019.ibw02.local>
2026-09-24 11:35:15 +0900

    add:峠 Case05.java テストコードの追加
    Case05のテストコードを作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -3,6 +3,8 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 
 /**
@@ -22,6 +25,12 @@ import org.openqa.selenium.By;
 @DisplayName("ケース05 キーワード検索 正常系")
 public class Case05 {
 
+
+	private final int PORT = 8080;
+	
+	private final String LOGIN_URL = "http://localhost:" + PORT + "/lms";
+	private final String KEY_WORD = "金";
+	
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
@@ -38,41 +47,96 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		goTo(LOGIN_URL);
+		
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		WebElement loginButtonElement = webDriver.findElement(By.cssSelector(".btn-primary "));
+		assertEquals("ログイン", loginButtonElement.getAttribute("value"));
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("ItTest2025");
+		webDriver.findElement(By.cssSelector(".btn-primary ")).click();
+		
+		pageLoadTimeout(30);
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.linkText("機能")).click();
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+
+		pageLoadTimeout(30);
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		
+		String originalWindow = webDriver.getWindowHandle();
+		
+		webDriver.findElement(By.linkText("よくある質問")).click();
+		
+		pageLoadTimeout(30);
+		Set<String> allWindows = webDriver.getWindowHandles();
+		 for (String windowHandle : allWindows) {
+             if (!originalWindow.contentEquals(windowHandle)) {
+            	 webDriver.switchTo().window(windowHandle);
+                 break;
+             }
+         }
+		
+		pageLoadTimeout(30);
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		
 	}
+	
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		// キーワードを入力
+		webDriver.findElement(By.id("form")).sendKeys(KEY_WORD);
+		// 検索
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[2]/div/input[1]")).click();
+		
+		pageLoadTimeout(30);
+		// チェック
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		assertEquals("助成金書類の作成方法が分かりません", webDriver.findElement(By.xpath("//*[@id=\"question-h[${status.index}]\"]/dt/span[2]")).getText());
+		assertEquals("1 件中 1 件から 1 件までを表示", webDriver.findElement(By.id("DataTables_Table_0_info")).getText());
+
+		getEvidence(new Object(){});
 	}
 	
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		// クリアボタン
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[2]/div/input[2]")).click();
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[2]/div/input[1]")).click();
+		
+		pageLoadTimeout(30);
+		// チェック
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		assertEquals("5 件中 1 件から 5 件までを表示", webDriver.findElement(By.id("DataTables_Table_0_info")).getText());
+
+		getEvidence(new Object(){});
 	}
 
 }
