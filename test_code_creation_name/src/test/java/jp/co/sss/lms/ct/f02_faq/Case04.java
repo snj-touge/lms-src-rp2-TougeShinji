9209d3bac99e1d1ab27a21df90f9f4723fa09c21
snj_toge <snj_toge@IBW-A25-019.ibw02.local>
2026-09-25 10:23:04 +0900

    fix:峠 Case04.java テストコードの修正
    test02のgoTo(LOGIN_URL)を削除

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -59,7 +59,7 @@ public class Case04 {
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		goTo(LOGIN_URL);
+		
 		//正しい入力値でログイン
 		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
 		webDriver.findElement(By.id("password")).sendKeys("ItTest2025");
